3e3f68cad37b6e80b2bd37ef2ff84449e1411a78
Weibo Zhang <theonlyhamstertoh@gmail.com>
2023-01-11 12:24:40 -0800

    fixed navbar

---
+++ app/process/head.tsx
@@ -0,0 +1,14 @@
+export default function Head() {
+  return (
+    <>
+      <title>Process | Moonwish Studios</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta charSet="utf-8" />
+      <meta
+        name="description"
+        content="Agile-oriented, our flexible process will get your professional and modern website running in no time"
+      />
+      <link rel="icon" href="/assets/brand/Logo.svg" />
+    </>
+  )
+}
