f0014903a1d9c994fb08d2784a5b7f7da0498eb4
Weibo Zhang <theonlyhamstertoh@gmail.com>
2023-01-11 12:13:41 -0800

    fixed schedule button

--- app/components/ContactBar.tsx
+++ app/components/ContactBar.tsx
@@ -1,8 +1,12 @@
 "use client"
 import React, { useState } from "react"
 import Button from "./Button"
+import dynamic from "next/dynamic"
 
-import { PopupButton } from "react-calendly"
+const PopupButton = dynamic(
+  () => import("react-calendly").then((calendly) => calendly.PopupButton),
+  { ssr: false }
+)
 export default function ContactBar() {
   return (
     <div className="flex flex-col justify-start w-full pt-20 pb-20 pl-12 space-y-6 text-left md:flex-row md:justify-center md:text-center bg-neutral-50 md:space-y-0 md:pt-36 md:pb-36">
@@ -10,14 +14,14 @@ export default function ContactBar() {
         <h1 className="mb-3 text-sm font-semibold text-lightestGray ">
           OR SCHEDULE A MEETING
         </h1>
-        {/* <PopupButton
+        <PopupButton
           url="https://calendly.com/mooonwishstudios/appointment"
           rootElement={document.getElementById("root")!}
           className="inline-block text-center bg-black hover:transition-all
           box-content rounded-md my-0  bg-opacity-0 hover:text-black hover:border-black
           border-gray border-2 text-lightestGray px-[18px] py-[8px] font-light"
           text="Schedule Meeting"
-        /> */}
+        />
       </div>
       <div className="flex flex-col items-start justify-center addressSection md:w-1/3 md:items-center ">
         <h1 className="text-sm font-semibold text-lightestGray ">ADDRESS</h1>
